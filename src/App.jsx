e4fbf36213436d93eced789d6d1a72c0625edb6c
Afraz33 <afraz3301@gmail.com>
2024-04-01 03:31:00 +0500

    style: Navbar profile button 1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 10);
+    }, 10500);
 
     return () => clearTimeout(timer);
   }, []);
